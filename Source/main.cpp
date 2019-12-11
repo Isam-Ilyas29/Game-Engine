3602faaeb76caa67a6ef0a22992716c959d2c121
Isam <isamilyas@hotmail.com>
2019-12-11 18:50:50 +0000

    Hello Window

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,12 +1,61 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
+
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-	std::cout << "Hello World!";
-	
-	glfwInit();
-	glfwTerminate();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
+int main(int argc, char* argv[]) {
+
+	//Initialises 'glfw core 3.3'
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//GLFW window creation
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My Game", NULL, NULL);
+	if (window == NULL) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//Loads all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//Render loop
+	while (!glfwWindowShouldClose(window)) {
+		//Poll Events checked 
+		processInput(window);
+
+		//Renders Screen Colour
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	//After leaving the loop we would need to terminate window
+	glfwTerminate();
 	return 0;
 }
+
+//
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+//[GLFW] Whenever the window is resized (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	//Makes sure the viewport matches the new window dimensions
+	glViewport(0, 0, width, height);
+}
+
